5e0aee86ee3080a1270b6c71db46b2db9eebc254
Nikhil Kumar Verma <86971607+NikVerAct@users.noreply.github.com>
2023-01-29 14:33:11 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class main{
+  public static void main(String[] args){
+      System.out.println("github");
+      for(int i=0; i<10; i++){
+          if(i==7){
+            System.out.println("Over at 7");
+          }
+        system.out.println(i);
+        }
+    }
+  }
